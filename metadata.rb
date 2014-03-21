855851012d6f6453afb08c21c1b5a292fb0c7145
Austin Heiman <atheiman@ksu.edu>
2014-03-21 10:08:56 -0500

    cookbook files

---
+++ metadata.rb
@@ -0,0 +1,8 @@
+name             'wpcas'
+maintainer       'YOUR_NAME'
+maintainer_email 'YOUR_EMAIL'
+license          'All rights reserved'
+description      'Installs/Configures wpcas'
+long_description IO.read(File.join(File.dirname(__FILE__), 'README.md'))
+version          '0.1.0'
+
